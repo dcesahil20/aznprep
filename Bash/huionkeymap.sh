f54f5b70dd5a387a1e34eed0064ad9e12efa219f
Sahil B <sahil@innowatts.com>
2021-10-06 13:29:19 +0530

    Adding bash files for graphic tablet

---
+++ Bash/huionkeymap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh                                                                                                                                                                                                                                    
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 1 "key  ctrl alt t"              
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 2 "key  ctrl e"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 3 "key  ctrl i"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 8 "key  ctrl j"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 9 "key  ctrl f"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 10 "key  ctrl k"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 11 "key  alt tab"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 12 "key  alt shift"            
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 13 "key  ctrl z"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 14 "key  ctrl y"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 15 "key  ctrl c"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 16 "key  ctrl v"                                                                                                                                                                                   
+xsetwacom --set 'HUION Huion Tablet_HS610 Pad pad' Button 17 "key  g"  
+xsetwacom --set 'HUION Huion Tablet_HS610 Touch Ring pad' AbsWheelDown  "key ctrl shift ="
+xsetwacom --set 'HUION Huion Tablet_HS610 Touch Ring pad' AbsWheelUp  "key ctrl -"
+                                                                                                                                                                                                                                                                                                                               
+                                                                                                                                                                     
+
---
+++ Bash/openboard.sh
@@ -0,0 +1,1 @@
+flatpak run ch.openboard.OpenBoard
